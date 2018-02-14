ea09f4d18a14edac33ab6a2bc9384c68645d3b85
Robbie Perry <robbieperry22@gmail.com>
2018-02-13 19:13:28 -0800

    Added logic to Configuration.cs that creates the 'Admin' and 'Member' roles if they don't already exist, creates 'a@a.a' and 'm@m.m' if they don't already exist, and assigns the roles.

--- LmycWebSite/Migrations/Configuration.cs
+++ LmycWebSite/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace LmycWebSite.Migrations
 {
+    using LmycWebSite.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -26,6 +29,63 @@ namespace LmycWebSite.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            //If roles don't yet exist, create them
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            string[] roleNames = { "Admin", "Member" };
+            IdentityResult roleResult;
+            foreach (var roleName in roleNames)
+            {
+                if (!RoleManager.RoleExists(roleName))
+                {
+                    roleResult = RoleManager.Create(new IdentityRole(roleName));
+                }
+            }
+
+            //Create a UserManager to add users and roles
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //Create a@a.a and m@m.m if users don't exist
+            if (context.Users.Where(u => u.Email == "a@a.a").Count() == 0)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "adminUser",
+                    Email = "a@a.a",
+                    FirstName = "adminFirstName",
+                    LastName = "adminLastName",
+                    Street = "adminStreet",
+                    City = "adminCity",
+                    Province = "adminProvince",
+                    PostalCode = "adminPostalCode",
+                    Country = "adminCountry",
+                    MobileNumber = "1234567890",
+                    SailingExperience = "Exptert"
+                };
+                UserManager.Create(user, "P@$$w0rd");
+            }
+            if (context.Users.Where(u => u.Email == "m@m.m").Count() == 0)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "memberUser",
+                    Email = "m@m.m",
+                    FirstName = "memberFirstName",
+                    LastName = "memberLastName",
+                    Street = "memberStreet",
+                    City = "memberCity",
+                    Province = "memberProvince",
+                    PostalCode = "memberPostalCode",
+                    Country = "memberCountry",
+                    MobileNumber = "1234567890",
+                    SailingExperience = "Exptert"
+                };
+                UserManager.Create(user, "P@$$w0rd");
+            }
+
+            //Assign roles to a@a.a and m@m.m
+            UserManager.AddToRole(context.Users.Where(u => u.Email == "a@a.a").First().Id, "Admin");
+            UserManager.AddToRole(context.Users.Where(u => u.Email == "m@m.m").First().Id, "Member");
         }
     }
 }
